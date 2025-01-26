08e6978ff60829f5dcedb3d3ec98bf53d5376102
xN3k <aadarshchaudhary463@gmail.com>
2025-01-26 16:15:57 +0545

    added exceptions and api services

---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:zesty/data/exception/app_exceptions.dart';
+import 'package:zesty/data/network/base_api_services.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkApiServices extends BaseApiServices {
+  @override
+  Future getApi(String url) async {
+    dynamic responseJson;
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 50));
+
+      if (response.statusCode == 200) {}
+
+      return responseJson;
+    } on SocketException {
+      throw NoInternetException("No Internet Connection");
+    } on TimeoutException {
+      throw FetchDataException('Timeout Try again');
+    }
+  }
+
+  dynamic returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 401:
+        throw BadRequestException(response.body.toString());
+      case 500:
+      case 404:
+        throw UnauthorizedException(response.body.toString());
+      default:
+        throw FetchDataException(
+            'Error occurred while communicating with server');
+    }
+  }
+}
